c6baf5037914dad93d86aa7a49b3c107a167f9c7
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-09-30 13:46:04 +0530

    Create practical9.py

---
+++ practical9.py
@@ -0,0 +1,9 @@
+#Q9
+#class and object
+class simpleclass:
+    def printer(self):
+        print("hello world")
+        
+s1 = simpleclass()
+s1.printer()
+print("Created By khizar shah Roll no 86 :>")
