6922c6b8838eaf30a653d617d247e96ff31414d2
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-09-30 13:47:02 +0530

    Create practical12.py

---
+++ practical12.py
@@ -0,0 +1,14 @@
+# Q12
+n = 7
+i = 2
+flag = 0
+while i<n :
+    if (n%i)==0:
+        print("not prime")
+        flag = 1
+        break
+    i+=1
+if flag == 0 :
+    print("it is prime number")
+print("Created By khizar shah Roll no 86 :>")
+    
