c6973e2f39cf8da93cbd15b4dcefaed4de960004
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-09-30 13:44:44 +0530

    Create practical6.py

---
+++ practical6.py
@@ -0,0 +1,6 @@
+#Q6 
+# dictionary is similar to maping a key to value like a map in many programiun language 
+dic = {"a" : "khizar",
+      "b" : "arshad"}
+print(dic)
+print("Created By khizar shah Roll no 86 :>")
