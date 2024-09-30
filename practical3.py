2cc3def17807f3eca5b234540c7962d93bd16c8b
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-09-30 13:43:49 +0530

    Create practical3.py

---
+++ practical3.py
@@ -0,0 +1,13 @@
+#Q3
+# createing list in python\
+list = [1,2,3,4,56]
+print (list)
+
+# remove
+list.remove(4)
+print(list)
+
+# append
+list.append(99)
+print(list)
+print("Created By khizar shah Roll no 86 :>")
